790d07078ca8de5169e6b7f09cc65f14230ce479
mvidi10 <marcosvidiri@gmail.com>
2020-08-25 09:46:44 -0300

    cambios

---
+++ public/js/home.js
@@ -0,0 +1,6 @@
+/*window.addEventListener("load", function (){
+    let nombre = prompt("Escriba su nombre, por favor.");    
+    document.querySelector(".bienvenido").innerHTML += ("¡Bienvenido, " + nombre + "!")
+
+})
+
